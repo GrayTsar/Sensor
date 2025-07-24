3d3735121703417ab10b04f255cc9238e9841597
Viatcheslav Ehrmann <viatcheslav.ehrmann@gmail.com>
2025-07-24 18:49:26 +0200

    #release 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,19 +9,24 @@ plugins {
 
 android {
     namespace = "com.graytsar.sensor"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.graytsar.sensor"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 8
-        versionName = "1.1.3"
+        targetSdk = 36
+        versionCode = 9
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
         release {
             isDebuggable = false
             isMinifyEnabled = true
@@ -48,35 +53,35 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
-    implementation("com.github.AAChartModel:AAChartCore-Kotlin:7.2.1")
+    implementation("com.github.AAChartModel:AAChartCore-Kotlin:7.4.0")
 
-    implementation("com.google.firebase:firebase-analytics-ktx:22.0.2")
-    implementation("com.google.firebase:firebase-crashlytics-ktx:19.0.3")
+    implementation("com.google.firebase:firebase-analytics-ktx:22.5.0")
+    implementation("com.google.firebase:firebase-crashlytics-ktx:19.4.4")
 
     //paging
-    implementation("androidx.paging:paging-runtime-ktx:3.3.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.6")
 
     //hilt
     implementation("androidx.hilt:hilt-common:1.2.0")
     implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.57")
+    ksp("com.google.dagger:hilt-android-compiler:2.57")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
 
     //room
-    implementation("androidx.room:room-paging:2.6.1")
+    implementation("androidx.room:room-paging:2.7.2")
 
     implementation(project(":repository"))
 
--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.graytsar.sensor.repository"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
@@ -37,20 +37,20 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
 
     //hilt
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.57")
+    ksp("com.google.dagger:hilt-android-compiler:2.57")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
 
     //room
-    implementation("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-paging:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.7.2")
+    implementation("androidx.room:room-runtime:2.7.2")
+    implementation("androidx.room:room-paging:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
